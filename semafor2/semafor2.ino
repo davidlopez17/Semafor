163b5f1347f8063d7ccd7a12a926de9a86064115
davidlopez17 <36242124+davidlopez17@users.noreply.github.com>
2018-05-25 17:56:18 +0200

    semafor

---
+++ semafor2/semafor2.ino
@@ -0,0 +1,107 @@
+/**********************************************************************************
+**                                                                               **
+**                              Array de 8 LEDs                                  **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const int ledR1 = 7;
+const int ledT1 = 8;
+const int ledV1 = 9;
+
+const int ledR2 = 10;
+const int ledT2 = 11;
+const int ledV2 = 12;
+
+
+//********** Setup ****************************************************************
+void setup()
+{
+  pinMode(ledR1 ,OUTPUT);
+  pinMode(ledT1 ,OUTPUT);
+  pinMode(ledV1 ,OUTPUT);
+  pinMode(ledR2 ,OUTPUT);
+  pinMode(ledT2 ,OUTPUT);
+  pinMode(ledV2 ,OUTPUT);
+  
+ 
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  digitalWrite(ledR1, HIGH);
+  digitalWrite(ledT1, LOW);
+  digitalWrite(ledV1, LOW);
+
+  digitalWrite(ledR2, HIGH);
+  digitalWrite(ledT2, LOW);
+  digitalWrite(ledV2, LOW);
+  delay (1000);
+  
+
+  digitalWrite(ledR1, HIGH);
+  digitalWrite(ledT1, LOW);
+  digitalWrite(ledV1, LOW);
+
+  digitalWrite(ledR2, LOW);
+  digitalWrite(ledT2, LOW);
+  digitalWrite(ledV2, HIGH);
+  delay(4000);
+
+  digitalWrite(ledR1, HIGH);
+  digitalWrite(ledT1, LOW);
+  digitalWrite(ledV1, LOW);
+
+  digitalWrite(ledR2, LOW);
+  digitalWrite(ledT2, HIGH);
+  digitalWrite(ledV2, LOW);
+  delay(1000);
+
+  digitalWrite(ledR1, HIGH);
+  digitalWrite(ledT1, LOW);
+  digitalWrite(ledV1, LOW);
+
+  digitalWrite(ledR2, HIGH);
+  digitalWrite(ledT2, LOW);
+  digitalWrite(ledV2, LOW);
+  delay(1000);
+
+  digitalWrite(ledR1, LOW);
+  digitalWrite(ledT1, LOW);
+  digitalWrite(ledV1, HIGH);
+
+  digitalWrite(ledR2, HIGH);
+  digitalWrite(ledT2, LOW);
+  digitalWrite(ledV2, LOW);
+  delay(4000);
+
+  digitalWrite(ledR1, LOW);
+  digitalWrite(ledT1, HIGH);
+  digitalWrite(ledV1, LOW);
+
+  digitalWrite(ledR2, HIGH);
+  digitalWrite(ledT2, LOW);
+  digitalWrite(ledV2, LOW);
+  delay(1000);
+
+
+
+ 
+
+
+  
+  
+ 
+
+
+
+
+  
+}
+
+
+//********** Funcions *************************************************************
